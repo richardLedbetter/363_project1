6986e73fa2a82fb8a7d064d8dc6ff77364f0e917
Natalia Gill <sngill.ca@gmail.com>
2020-04-08 18:16:15 -0700

    Add files via upload

--- src/disk_store/HashIndex.java
+++ src/disk_store/HashIndex.java
@@ -1,10 +1,25 @@
 package disk_store;
 
+/* HASH ;
+ * 
+ * <bucket, list>
+ * 
+ * -inf to + inf - > mod 10 = 0 to 10
+ * 
+ * {0,1,2,3}
+ * SET A    SET B
+ * {0,2} -> {2}
+ * {1,3} -> {3}
+ * 
+ * 
+ * 
+ */
 
 import java.util.List;
+import java.util.*;
 
 /**
- * A hash index.  
+ * A hash index.
  * 
  */
 
@@ -13,25 +28,69 @@ public class HashIndex implements DBIndex {
 	/**
 	 * Create an new index.
 	 */
+	HashMap<Integer, ArrayList<Integer>> block;
+
 	public HashIndex() {
-		throw new UnsupportedOperationException();
+		block = new HashMap<>();
+		// throw new UnsupportedOperationException();
 	}
-	
+
 	@Override
 	public List<Integer> lookup(int key) {
-		throw new UnsupportedOperationException();
+		ArrayList<Integer> list = block.get(key);
+		if (list != null) {
+			// return value at position
+			return list;
+		} else {
+			return new ArrayList<>();
+		}
+		// throw new UnsupportedOperationException();
 	}
-	
+
 	@Override
 	public void insert(int key, int blockNum) {
-		throw new UnsupportedOperationException();
+		// check if exists in HM
+
+		System.out.println("SIZE: --- " + block.size());
+		if (block.containsKey(key) == false) {
+			ArrayList<Integer> list = new ArrayList<>();
+			list.add(blockNum);
+			block.put(key, list);
+		} else {
+			block.get(key).add(blockNum);
+		}
+		// throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void delete(int key, int blockNum) {
-		throw new UnsupportedOperationException();
+
+		ArrayList<Integer> list = block.get(key);
+
+		if (list != null) {
+			list.remove(blockNum);
+
+			if (list.size() <= 0) {
+				block.remove(key);
+			}
+		}
+		// throw new UnsupportedOperationException();
+
 	}
-	
+
+	public int size() {
+		// return block.size();
+		int count = 0;
+
+		for (Integer key : block.keySet()) {
+
+			count += block.get(key).size();
+
+		}
+		return count;
+		// throw new UnsupportedOperationException();
+	}
+
 	@Override
 	public String toString() {
 		throw new UnsupportedOperationException();
