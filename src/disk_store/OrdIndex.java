28b23e2d21b142a13fdc187c61af53803eee8f4c
Richard Ledbetter <rdosrun@gmail.com>
2020-04-12 17:50:47 -0700

    with tree searching

--- src/disk_store/OrdIndex.java
+++ src/disk_store/OrdIndex.java
@@ -4,6 +4,7 @@ package disk_store;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -23,50 +24,99 @@ public class OrdIndex implements DBIndex {
 	/**
 	 * Create an new ordered index.
 	 */
-	ArrayList<ArrayList<Integer>> index;
+	ArrayList<extra> index;
 	
 	public OrdIndex() {
-		index = new ArrayList<ArrayList<Integer> >(0);
+		index = new ArrayList<extra>(0);
+		
 	}
+	
+	public int indexof(int key) {
+		if(index.isEmpty()) {
+			return -1;
+		}
+		int curr_pos = (index.size())/2;
+		System.out.println("========new search========");
+		System.out.println("looking for: " + key);
+		System.out.println("start: "+curr_pos);
+		int inc = curr_pos;	
+		while(inc>=0) {
+			System.out.print("current_pos: "+curr_pos);
+			extra tmp = index.get(curr_pos);
+			System.out.println("  ,  current_key:"+tmp.key);
+			inc = inc/2;
+			if(tmp.key == key ) {
+				return curr_pos;
+			}else if(inc==0){
+				if(curr_pos!=0) {
+					tmp = index.get(curr_pos-1);
+				}				
+				if(tmp.key==key) {
+					System.out.print("current_pos: "+(curr_pos-1));
+					System.out.println("  ,  returning:"+tmp.key);
+					return curr_pos-1;
+				}else {
+					if(curr_pos<index.size()-1) {
+						tmp = index.get(curr_pos+1);
+					}			
+					if(tmp.key==key) {
+						System.out.print("current_pos: "+(curr_pos-1));
+						System.out.println("  ,  returning:"+tmp.key);
+						return curr_pos+1;
+					}
+				}
+				return -1;
+			}else if(tmp.key<key){
+				curr_pos = curr_pos+inc;
+			}else {				
+				curr_pos = curr_pos-inc;
+			}
+		}		
+		return -1;
+	}
+
 	
 	@Override
 	public List<Integer> lookup(int key) {
 		
-		key--;
-		if(key>=index.size()) {
+		if(indexof(key)==-1) {
 			return new ArrayList<Integer>();
 		}
-		Set<Integer> set = new HashSet<>(index.get(key));
+		Set<Integer> set = new HashSet<>();
+		set.addAll(index.get(indexof(key)).val);
 		ArrayList<Integer> tmp = new ArrayList<Integer>(set);
 		return tmp;
 	}
 	
 	@Override
 	public void insert(int key, int blockNum) {
-		key--;
-		if(key>=index.size()) {
-			ArrayList <Integer> tmp = new ArrayList<>();
-			tmp.add(blockNum);
+		
+		if(indexof(key)==-1) {
+			extra tmp = new extra();
+			tmp.val = new ArrayList<>();
+			tmp.val.add(blockNum);
+			tmp.key = key;
 			index.add(tmp);
 			return;
 		}else {
-			int test = index.get(key).indexOf(blockNum);
-			index.get(key).add(blockNum);
-			Collections.sort(index.get(key));		
+			index.get(indexof(key)).val.add(blockNum);
+			Collections.sort(index.get(indexof(key)).val);
 		}	
 	}
 
 	@Override
 	public void delete(int key, int blockNum) {
-		key--;
 		if(index.size()<=key) {
 			return;
 		}else {
-			int l = index.get(key).indexOf(blockNum);
-			if(l==-1) {
+			int big = indexof(key);			
+			int location = index.get(big).indexof(blockNum);
+			System.out.println("===lookup==="+location);
+			if(location == -1) {		
 				return;
 			}
-			index.get(key).remove(l);
+			System.out.println("===lookup===" + location);
+			index.get(big).val.remove(location);
 		}
 		
 	}
@@ -78,10 +128,8 @@ public class OrdIndex implements DBIndex {
 	public int size() {
 		// you may find it useful to implement this
 		int count =0;
-		for(ArrayList<Integer> i: index) {
-			for(Integer j : i) {
-				count++;
-			}
+		for(extra i: index) {
+			count = count + i.val.size();
 		}
 		return count;
 	}
@@ -91,3 +139,55 @@ public class OrdIndex implements DBIndex {
 		throw new UnsupportedOperationException();
 	}
 }
+
+class extra{
+	int key;
+	ArrayList<Integer> val;
+	public int indexof(int key) {
+		if(val.isEmpty()) {
+			return -1;
+		}
+		int curr_pos = (val.size())/2;
+		System.out.println("========new search========");
+		System.out.println("looking for: " + key);
+		System.out.println("start: "+curr_pos);
+		int inc = curr_pos;	
+		while(inc>=0) {
+			System.out.print("current_pos: "+curr_pos);
+			int tmp = val.get(curr_pos);
+			System.out.println("  ,  current_key:"+tmp);
+			inc = inc/2;
+			if(tmp == key ) {
+				return curr_pos;
+			}else if(inc==0){
+				if(curr_pos!=0) {
+					tmp = val.get(curr_pos-1);
+				}				
+				if(tmp==key) {
+					System.out.print("current_pos: "+(curr_pos-1));
+					System.out.println("  ,  returning:"+tmp);
+					return curr_pos-1;
+				}else {
+					if(curr_pos<val.size()-1) {
+						tmp = val.get(curr_pos+1);
+					}			
+					if(tmp==key) {
+						System.out.print("current_pos: "+(curr_pos+1));
+						System.out.println("  ,  returning:"+tmp);
+						return curr_pos+1;
+					}
+				}
+				return -1;
+			}else if(tmp<key){
+				curr_pos = curr_pos+inc;
+			}else {				
+				curr_pos = curr_pos-inc;
+			}
+		}		
+		return -1;
+	}
+	public boolean equals(int k) {
+		return key==k;
+	}
+}
+
